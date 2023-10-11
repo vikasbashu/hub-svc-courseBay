8dc1735acb9ea238d3a96b9c0a18cdfd33de7925
Vikas Sharma <70948583+vikasbashu@users.noreply.github.com>
2023-10-11 15:34:43 +0530

    added docker container setup

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const auth_1 = require("./middleware/auth");
 const app = (0, express_1.default)();
-const portNumber = 443;
+const portNumber = 3000;
 app.use((0, cors_1.default)());
 app.use(auth_1.authenticator_middleWare);
 app.use(express_1.default.json());
@@ -19,7 +19,7 @@ app.use("/admin", admin_1.router);
 app.use("/users", user_1.router);
 app.use("/util", utility_1.router);
 try {
-    mongoose_1.default.connect('mongodb+srv://vikasbashu:Cb21DWHdt02WyvIc@tycers.o6r8vsu.mongodb.net/CourseBay?retryWrites=true&w=majority');
+    mongoose_1.default.connect('mongodb://localhost:27018/courses');
 }
 catch (error) {
     console.log(error.message);
